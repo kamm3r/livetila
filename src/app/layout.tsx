a159c8e418a37edc3acb8cb49aab4cd9cd7a27c7
kamm3r <marcokammer59@gmail.com>
2024-09-12 18:41:46 +0300

    updated dependencies, added geist and removed extra div from frontpage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import "~/styles/globals.css";
 
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { ThemeProvider } from "~/@/components/theme-provider";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Livetila",
   description: "live.tuloslista API, live result overlay for Live streams",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
@@ -18,12 +15,10 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`font-sans ${inter.variable}`}>
+    <html lang="en" className={`font-sans ${GeistSans.variable}`}>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
