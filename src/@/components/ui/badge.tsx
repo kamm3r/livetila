f79fcdbdf4c7b07af9ee91e1329f5769dac981c6
kamm3r <marcokammer59@gmail.com>
2026-01-08 14:07:40 +0200

    feat: added animation to progress badge to pulse

--- src/@/components/ui/badge.tsx
+++ src/@/components/ui/badge.tsx
@@ -21,13 +21,13 @@ const badgeVariants = cva(
 				link: "text-primary underline-offset-4 hover:underline",
 
 				unallocated:
-					"bg-yellow-500/10 text-yellow-500 border-yellow-500/20 [a]:hover:bg-yellow-500/20",
+					"border-yellow-500/20 bg-yellow-500/10 text-yellow-500 [a]:hover:bg-yellow-500/20",
 				allocated:
-					"bg-cyan-500/10 text-cyan-500 border-cyan-500/20 [a]:hover:bg-cyan-500/20",
+					"border-cyan-500/20 bg-cyan-500/10 text-cyan-500 [a]:hover:bg-cyan-500/20",
 				progress:
-					"bg-blue-500/10 text-blue-500 border-blue-500/20 [a]:hover:bg-blue-500/20",
+					"animate-pulse border-blue-500/20 bg-blue-500/10 text-blue-500 [a]:hover:bg-blue-500/20",
 				official:
-					"bg-green-500/10 text-green-500 border-green-500/20 [a]:hover:bg-green-500/20",
+					"border-green-500/20 bg-green-500/10 text-green-500 [a]:hover:bg-green-500/20",
 			},
 		},
 		defaultVariants: {
