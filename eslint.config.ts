75372a515d3bc401749aab0d5392574e568d9b7c
kamm3r <marcokammer59@gmail.com>
2026-06-04 13:48:08 +0300

    migrate from biome to eslint and prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,49 +1,49 @@
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals'
 
 export default defineConfig(
-  ...nextVitals,
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
+  { ignores: [".svelte-kit/**", "build/**", "node_modules/**"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  ...svelte.configs["flat/recommended"],
   {
-    files: ["**/*.ts", "**/*.tsx"],
-    extends: [
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  {
+    files: ["**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+  },
+  {
     rules: {
+      "no-undef": "off",
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
         { prefer: "type-imports", fixStyle: "inline-type-imports" },
       ],
+      "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": [
         "warn",
         { argsIgnorePattern: "^_" },
       ],
       "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        { checksVoidReturn: { attributes: false } },
-      ],
+      "svelte/no-navigation-without-resolve": "off",
+      "svelte/no-unused-svelte-ignore": "off",
+      "svelte/prefer-writable-derived": "off",
+      "svelte/prefer-svelte-reactivity": "off",
+      "svelte/require-each-key": "warn",
     },
   },
-  {
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
-    },
-  },
+  { linterOptions: { reportUnusedDisableDirectives: true } },
 );
--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,13 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-svelte"],
+  tailwindStylesheet: "./src/routes/layout.css",
+  overrides: [
+    {
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
+      },
+    },
+  ],
 };
