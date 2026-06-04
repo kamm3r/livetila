9806e0c426ea42d005f29b44624765e6b83b25a1
kamm3r <marcokammer59@gmail.com>
2026-06-04 22:14:45 +0300

    chore: remove layout.css reference from prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-svelte"],
-  tailwindStylesheet: "./src/routes/layout.css",
   overrides: [
     {
       files: "*.svelte",
